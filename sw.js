9d8babde90428fac4f518bbd9b25298c04bc5346
nton4976-wq <nton4976@gmail.com>
2026-03-02 17:21:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,10 @@
-const CACHE_NAME = "sap-cache-v1";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/maskable-512.png"
-];
-
-// Install: cache core files
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
-  );
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-// Activate: cleanup old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Fetch: network-first for API calls, cache-first for static
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  // Always go to network for Google Apps Script API
-  if (url.hostname.includes("script.google.com")) {
-    event.respondWith(fetch(req).catch(() => caches.match(req)));
-    return;
-  }
-
-  event.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req))
-  );
+self.addEventListener("fetch", event => {
 });
